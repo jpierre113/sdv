b77dd457350739d9180e8bc915a8c2ddc35b0a01
Janna Pierre <gojanna@aol.com>
2021-02-28 19:26:15 -0500

    Rendered a character img to start and updated README

---
+++ src/iconimg.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Iconimg extends Component{
+  render(){
+    return(
+      <img src='./robin.png' alt="Character's Appearance" />
+    );
+  }
+}
+
+export default Iconimg;
